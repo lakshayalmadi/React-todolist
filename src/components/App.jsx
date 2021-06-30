aaeed992b9e8bdddc7271361898206dab45368f8
lakshayalmadi <lashay.almadi.la@gmail.com>
2021-06-30 17:58:01 +0530

    delete-item-feature

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import ToDoItem from "./ToDoItem";
 
 function App() {
   const [inputText, setInputText] = useState("");
@@ -8,12 +9,21 @@ function App() {
     const newValue = event.target.value;
     setInputText(newValue);
   }
+
   function addItem() {
     setItems((prevItems) => {
       return [...prevItems, inputText];
     });
     setInputText("");
   }
+  function deleteItem(id) {
+    setItems((prevItems) => {
+      return prevItems.filter((item, index) => {
+        return index !== id;
+      });
+    });
+  }
+
   return (
     <div className="container">
       <div className="heading">
@@ -27,8 +37,13 @@ function App() {
       </div>
       <div>
         <ul>
-          {items.map((todoItem) => (
-            <li>{todoItem}</li>
+          {items.map((todoItem, index) => (
+            <ToDoItem
+              key={index}
+              id={index}
+              text={todoItem}
+              onChecked={deleteItem}
+            />
           ))}
         </ul>
       </div>
